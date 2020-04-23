ef96685aa3e6d563ce986950f1ef79c195728adb
eaarda <elifaarda@gmail.com>
2020-04-23 19:12:14 +0300

    css eklenti

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,7 @@ import layoutcss from './assets/css/layout.css'
 import stylecss from './assets/css/style.css'
 import ordercss from './assets/css/bundle-order.css'
 import maincss from './assets/css/bundle-main.css'
+import categorycss from './assets/css/bundle-category.css'
 
 export default {
   name: 'app',
